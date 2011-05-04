252a8834599a829b41737f55472d74847c1a9090
Eugeni Dodonov <eugeni@mandriva.com>
2011-05-04 14:51:43 -0300

    implement chunks saving

--- openclass/screen.py
+++ openclass/screen.py
@@ -5,7 +5,7 @@ from gtk import gdk
 
 class Screen:
     """Screen capturing class"""
-    def __init__(self, width=None, height=None, target_width=None, target_height=None):
+    def __init__(self, width=None, height=None):
         if not width:
             width = gdk.screen_width()
         self.width = width
@@ -13,39 +13,47 @@ class Screen:
             height = gdk.screen_height()
             self.height = height
 
-        if not target_width:
-            target_width = 800
-        self.target_width = target_width
-        if not target_height:
-            target_height = 480
-        self.target_height = target_height
-
-    def capture(self):
-        """Captures a screenshot"""
+    def capture(self, scale_x=None, scale_y=None, quality=75, raw=False):
+        """Captures a screenshot and returns in for width, height, data"""
 
         screenshot = gdk.Pixbuf(gdk.COLORSPACE_RGB, False, 8, self.width, self.height)
         screenshot.get_from_drawable(gdk.get_default_root_window(),
                                     gdk.colormap_get_system(),
                                     0, 0, 0, 0,
                                     self.width, self.height)
-        if self.width != self.target_width or self.height != self.target_height:
-            screenshot = screenshot.scale_simple(self.target_width, self.target_height, gdk.INTERP_BILINEAR)
 
-        # process screenshot
-        # screenshot.save_to_callback(...)
-        sp = screenshot.subpixbuf(0, 0, 320, 240)
-        screenshot.save("tela.jpg", "jpeg", {"quality": "75"})
-        sp.save("sp.jpg", "jpeg", {"quality": "75"})
-        print screenshot.get_pixels().__hash__()
-        s2 = screenshot.copy()
-        print s2.get_pixels().__hash__()
-        print sp.get_pixels().__hash__()
-        #data = screenshot.get_pixels()
+        if not scale_x:
+            scale_x = self.width
+        if not scale_y:
+            scale_y = self.height
+        if self.width != scale_x or self.height != scale_y:
+            screenshot = screenshot.scale_simple(scale_x, scale_y, gdk.INTERP_BILINEAR)
 
-        # vamos tentar carregar agora
-        data = open("sp.jpg").read()
-        loader = gdk.PixbufLoader(image_type="jpeg")
-        loader.write(data)
-        loader.close()
-        pb = loader.get_pixbuf()
-        pb.save("sp2.jpg", "jpeg", {"quality": "30"})
+        if raw:
+            return scale_x, scale_y, screenshot
+        else:
+            image=[]
+            screenshot.save_to_callback(lambda buf, image: image.append(buf), "jpeg", {"quality": str(quality)}, image)
+            return scale_x, scale_y, "".join(image)
+
+    def chunks(self, chunks_x=4, chunks_y=4, quality=75):
+        """Captures a screenshot and converts it into a serie of smaller shots"""
+
+        width, height, image = self.capture(raw=True)
+
+        step_x = width / chunks_x
+        step_y = height / chunks_y
+
+        chunks=[]
+
+        print "%d %d" % (width, height)
+        for y in range(chunks_y):
+            pos_y = y * step_y
+            for x in range(chunks_x):
+                pos_x = x * step_x
+
+                chunk = image.subpixbuf(pos_x, pos_y, step_x, step_y)
+                img = []
+                chunk.save_to_callback(lambda buf, img: img.append(buf), "jpeg", {"quality": str(quality)}, img)
+                chunks.append((pos_x, pos_y, step_x, step_y, "".join(img)))
+        return chunks
