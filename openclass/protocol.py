589ab886895b56b3430d56cce2c78fbab898a256
Eugeni Dodonov <eugeni@mandriva.com>
2011-05-05 18:51:29 -0300

    properly calculate size

--- openclass/protocol.py
+++ openclass/protocol.py
@@ -77,7 +77,7 @@ class Protocol:
 
     def unpack_chunk(self, data):
         """Unpacks a chunk of data"""
-        pos_x, pos_y, step_x, step_y = struct.unpack("!iiii", data)
         head_size = struct.calcsize("!iiii")
+        pos_x, pos_y, step_x, step_y = struct.unpack("!iiii", data[:head_size])
         return pos_x, pos_y, step_x, step_y, data[head_size:]
 
