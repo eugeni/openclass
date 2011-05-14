7624a61922f0113c0f1c84cda5ffe9f3933bf4af
Eugeni Dodonov <eugeni@mandriva.com>
2011-05-13 22:51:09 -0300

    proper license

--- teacher.py
+++ teacher.py
@@ -4,7 +4,18 @@ OpenClass teacher module.
 
 Copyright, (C) Eugeni Dodonov <eugeni@dodonov.net>, 2008-2011
 
-Distributed under GPLv2 license, please see COPYING for details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 """
 
